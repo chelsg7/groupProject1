72aa56785f260c748818a31d7c3378823e6231d9
rishikesh18 <rishi.neupane@gmail.com>
2018-04-10 10:18:45 -0400

    done with weather stuff1

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,8 +1,9 @@
-function getCity(city) {
+function getCity() {
+    var city = "nc/cary";
 
 
-var queryURL = "http://api.wunderground.com/api/e58ab528f4132c06/forecast/q/"+city+".json";
-//var queryURL2 = "http://api.wunderground.com/api/e58ab528f4132c06/conditions/q/np/syangja.json"
+var queryURL = "http://api.wunderground.com/api/87d18b0282c9396c/forecast/q/"+city+".json"; 
+
 
 $.ajax({
     url: queryURL,
@@ -22,6 +23,7 @@ $.ajax({
        console.log(x);
         $("#duration1").html(response.forecast.txt_forecast.forecastday[0].title);
         $("#forecat_first").html(x);
+        $("#forecat_first_description").html(response.forecast.txt_forecast.forecastday[0].fcttext);
         
         var y = document.createElement("IMG");
         y.setAttribute("src", response.forecast.txt_forecast.forecastday[1].icon_url);
@@ -32,6 +34,7 @@ $.ajax({
        console.log(y);
         $("#duration2").html(response.forecast.txt_forecast.forecastday[1].title);
         $("#forecat_second").html(y);
+        $("#forecat_second_description").html(response.forecast.txt_forecast.forecastday[1].fcttext);
         
         var z = document.createElement("IMG");
         z.setAttribute("src", response.forecast.txt_forecast.forecastday[2].icon_url);
@@ -42,12 +45,42 @@ $.ajax({
        console.log(z);
         $("#duration3").html(response.forecast.txt_forecast.forecastday[2].title);
         $("#forecat_third").html(z);
+        $("#forecat_third_description").html(response.forecast.txt_forecast.forecastday[2].fcttext);
+       
+    });
+
+    var queryURL2 = "http://api.wunderground.com/api/87d18b0282c9396c/conditions/q/"+city+".json"
+
+$.ajax({
+    url: queryURL2,
+    method: "GET"
+    })
+    
+    .then (function(response) {
+        console.log(response);
+       
+        var x = document.createElement("IMG");
+        x.setAttribute("src", response.current_observation.icon_url);
+        x.setAttribute("width", "70");
+        x.setAttribute("height", "auto");
+        x.setAttribute("alt", "The Pulpit Rock");
+       // document.body.appendChild(x);
+       console.log(x);
+       // $("#duration1").html(response.forecast.txt_forecast.forecastday[0].title);
+        $("#current_condition").html(response.current_observation.weather); 
+        $("#current_img").html(x);
+        $("#current_data").html("<p>Temperature: "+response.current_observation.temperature_string + "</p></br>"+
+                                "<p>Feels like: "+response.current_observation.feelslike_f+" F (" + response.current_observation.feelslike_c+ " C)</p></br>"+
+                                "<p>Precipitation today (inch): "+response.current_observation.precip_today_in+ "</p></br>");
+        $("#current_data2").html("<p>Relative Humidity: "+response.current_observation.relative_humidity + "</p></br>"+
+                                "<p>Wind: "+response.current_observation.wind_string + "</p></br>"+
+                                "<p>Visibility: "+response.current_observation.visibility_km + " km</p></br>"+
+                                "<p>Atmospheric Pressure (mb): "+response.current_observation.pressure_mb+ "</p></br>");
+        
        // $("#forecat_third").html("</br><p>Day: " +response.forecast.txt_forecast.forecastday[0].fcttext + 
        // "</br>" + response.forecast.txt_forecast.forecastday[0].fcttext_metric +  "</p>");
        // console.log("<img>" + response.forecast.txt_forecast.forecastday[0].icon_url +">");
     });
-    
-
 };
 
 $("#submit").click(function(){
@@ -56,15 +89,9 @@ $("#submit").click(function(){
    
   });
 
-
-
-
 function getLocation() {
    
-        navigator.geolocation.getCurrentPosition(showPosition);
-
-    
-        
+        navigator.geolocation.getCurrentPosition(showPosition);        
 }
  
 
@@ -83,10 +110,10 @@ $.ajax({
     
     .then (function(response) {
         console.log(response);
-        console.log(response.results[0].address_components[5].short_name);
-        console.log(response.results[0].address_components[2].long_name);
-        state=response.results[0].address_components[5].short_name;
-        cityName=response.results[0].address_components[2].long_name;
+        console.log(response.results[4].address_components[2].short_name);
+        console.log(response.results[4].address_components[0].long_name);
+        state=response.results[4].address_components[2].short_name;
+        cityName=response.results[4].address_components[0].long_name;
         var city= state+"/"+cityName;
         getCity(city);
     });
@@ -101,7 +128,8 @@ $.ajax({
 $( document ).ready(function() {
 
     if (navigator.geolocation) {
-         getLocation();
+      //   getLocation();
+         getCity();
     }
     else {
         console.log("not available");
