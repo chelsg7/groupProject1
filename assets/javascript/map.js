defa1f01767577491fddc2233e1907f3c5950ba5
Edward Nimocks <enimocks@gmail.com>
2018-04-13 14:10:41 -0400

    formatting

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -40,6 +40,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
 }
+
 function showPins(lat, lng) {
 
   var queryURL = "https://trailapi-trailapi.p.mashape.com/?lat=" + lat + "&limit=10&lon=" + lng + "&radius=25";
@@ -78,54 +79,59 @@ function showPins(lat, lng) {
     }
   })
 }
+
 function userSearch(city, state) {
 
   var queryURL = "https://trailapi-trailapi.p.mashape.com/?limit=10&q[city_cont]=" + city + "&q[state_cont]=" + state + "&radius=25"
   $.ajax({
-  url: queryURL,
-  dataType: 'json',
-  headers: {"X-Mashape-Key": "sa38JqfW39msh5UFDYOe0Y44RTAEp1jP9K1jsnk52st50qIIvw",
-  "Accept": "text/plain"},
-  method: "get",
-  contentType: 'application/json; charset=utf-8',
-  success: function (response) {
-    console.log(response);
-    var myLatLng = { lat: 35.851, lng: -78.796 };
-    bounds  = new google.maps.LatLngBounds();
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: 35.851, lng: -78.796 },
-      zoom: 6
-    });
-    for (var i = 0; i < 10; i++) {
+    url: queryURL,
+    dataType: 'json',
+    headers: {
+      "X-Mashape-Key": "sa38JqfW39msh5UFDYOe0Y44RTAEp1jP9K1jsnk52st50qIIvw",
+      "Accept": "text/plain"
+    },
+    method: "get",
+    contentType: 'application/json; charset=utf-8',
+    success: function (response) {
+      console.log(response);
+      var myLatLng = { lat: 35.851, lng: -78.796 };
+      bounds = new google.maps.LatLngBounds();
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 35.851, lng: -78.796 },
+        zoom: 6
+      });
+      for (var i = 0; i < 10; i++) {
         var lat = response.places[i].lat;
         latArray.push(lat);
         var lng = response.places[i].lon;
         lngArray.push(lng);
         $("#trails").append(response.places[i].name + "<br>")
-      //   var myLatLng = { lat: parseInt(lat), lng: parseInt(lng)};
-          var marker = new google.maps.Marker({
+        //   var myLatLng = { lat: parseInt(lat), lng: parseInt(lng)};
+        var marker = new google.maps.Marker({
           position: new google.maps.LatLng(lat, lng),
           map: map,
           title: response.places[i].name
         })
         if (i === 0) {
-          var laLatLng = new google.maps.LatLng((response.places[i].lat),(response.places[i].lon));
+          var laLatLng = new google.maps.LatLng((response.places[i].lat), (response.places[i].lon));
           map.panTo(laLatLng);
           map.setZoom(9);
-      }
+        }
         console.log(marker.position)
-        $("#names"+i).html(response.places[i].name)
-        $("#location"+i).html(response.places[i].city + ", " + response.places[i].state)
-        $("#direction"+i).html(response.places[i].directions)
+        $("#names" + i).html(response.places[i].name)
+        $("#location" + i).html(response.places[i].city + ", " + response.places[i].state)
+        $("#direction" + i).html(response.places[i].directions)
+      }
     }
-  }})}
-    
-    $(document).ready(function () {
-      $("#search").submit(function(event) {
-        event.preventDefault();
-        var cityInput = ($(".inputCity").val().trim());
-        var stateInput = ($(".inputState").val().trim());
-        console.log(cityInput + stateInput);
-        userSearch(cityInput,stateInput)
-      });
-      })
+  })
+}
+
+$(document).ready(function () {
+  $("#search").submit(function (event) {
+    event.preventDefault();
+    var cityInput = ($(".inputCity").val().trim());
+    var stateInput = ($(".inputState").val().trim());
+    console.log(cityInput + stateInput);
+    userSearch(cityInput, stateInput)
+  });
+})
